ca53068d6e8ede4c7873ff4300c2b0005e571131
Ancientshield <jacklin2100@gmail.com>
2023-08-13 23:30:21 +0800

    this course file is wrong

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,31 @@
 <template>
 	<div class="about">
 		<h1>{{ $route.params.member }}</h1>
+		<p>{{ store.counter }}</p>
+		<p>{{ store.doubleCount }}</p>
+		<button type="button" @click.prevent="store.increment">Press me!</button>
 	</div>
 </template>
 
 <script>
 	import { useRoute, useRouter } from 'vue-router';
+	import { useCounterStore } from '../stores/counter.js';
 
 	export default {
 		setup() {
 			const route = useRoute(); // this.$route 回傳的是路徑 path
 			const router = useRouter(); // this.$router 回傳的是實例 object
+			const store = useCounterStore();
 
 			console.log(route.params.member);
 
 			router.push({
 				hash: '#test',
 			});
+
+			return {
+				store,
+			};
 		},
 	};
 </script>
