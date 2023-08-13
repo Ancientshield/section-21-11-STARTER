99f017b3b3a375d14369a242cbe5e87a4daa2db2
Ancientshield <jacklin2100@gmail.com>
2023-08-13 19:50:17 +0800

    finished Router Hooks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>{{ $route.params.member }}</h1>
+	</div>
 </template>
+
+<script>
+	import { useRoute, useRouter } from 'vue-router';
+
+	export default {
+		setup() {
+			const route = useRoute(); // this.$route 回傳的是路徑 path
+			const router = useRouter(); // this.$router 回傳的是實例 object
+
+			console.log(route.params.member);
+
+			router.push({
+				hash: '#test',
+			});
+		},
+	};
+</script>
